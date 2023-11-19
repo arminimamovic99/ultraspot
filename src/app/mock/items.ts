8f513b53cedf247294bc5297a73b98539c4407ea
arminimamovic99 <aimamovic99@outlook.com>
2023-11-19 21:47:23 +0100

    hookah configuration flow

--- src/app/mock/items.ts
+++ src/app/mock/items.ts
@@ -2,63 +2,63 @@ export const drinks =  [
   {
     id: 1,
     name: 'Espresso',
-    price: '2KM',
+    price: 2,
     imgSrc: 'assets/drink2.png',
     type: 1
   },
   {
     id: 2,
     name: 'Caj',
-    price: '1.5KM',
+    price: 1.5,
     imgSrc: 'assets/drink2.png',
     type: 1
   },
   {
     id: 3,
     name: 'Coca-cola',
-    price: '2.5KM',
+    price: 2.5,
     imgSrc: 'assets/glass.png',
     type: 1
   },
   {
     id: 4,
     name: 'Coca-cola zero',
-    price: '2.5KM',
+    price: 2.5,
     imgSrc: 'assets/glass.png',
     type: 1
   },
   {
     id: 5,
     name: 'Kiseljak',
-    price: '1.5KM',
+    price: 1.5,
     imgSrc: 'assets/glass.png',
     type: 1
   },
   {
     id: 6,
     name: 'Exotic',
-    price: '2KM',
+    price: 2,
     imgSrc: 'assets/glass.png',
     type: 1
   },
   {
     id: 7,
     name: 'Schweppes Bitter',
-    price: '3KM',
+    price: 3,
     imgSrc: 'assets/glass.png',
     type: 1
   },
   {
     id: 8,
     name: 'Schweppes Tangerine',
-    price: '3KM',
+    price: 3,
     imgSrc: 'assets/glass.png',
     type: 1
   },
   {
     id: 9,
     name: 'Schweppes Tonic',
-    price: '3KM',
+    price: 3,
     imgSrc: 'assets/glass.png',
     type: 1
   }
@@ -68,22 +68,33 @@ export const hookahs =  [
   {
     id: 10,
     name: 'Nargila Amy Deluxe',
-    price: '7KM',
+    price: 7,
     imgSrc: 'assets/hookahicon4.png',
     type: 2
   },
   {
     id: 11,
     name: 'Nargila Khalil',
-    price: '7KM',
+    price: 7,
     imgSrc: 'assets/hookahicon4.png',
     type: 2
   },
   {
     id: 12,
     name: 'Nargila Yahya',
-    price: '7KM',
+    price: 7,
     imgSrc: 'assets/hookahicon4.png',
     type: 2
   }
 ]
+
+export const flavors = [
+  'Swiss',
+  'Strong',
+  'Ice',
+  'Mint',
+  'Lady killer',
+  'Nar',
+  'Borovnica',
+  'Grozdje'
+];
